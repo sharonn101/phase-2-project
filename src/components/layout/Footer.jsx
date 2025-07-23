4e94c21023ac88911599b8ccb7701feef805e82a
elnabaseugine <elnabaseugine@gmail.com>
2025-07-23 18:05:45 +0300

    remove year from copyright notice in Footer component

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer>
-      <p>&copy; 2024 Inventory Management</p>
+      <p>&copy;  Inventory Management</p>
     </footer>
   );
 }
