e01f22d247b4c0c1d3b2060a997e1258d4334d13
Sharon <sharonirungu2006@gmail.com>
2025-07-23 09:28:43 +0300

    fetch data

---
+++ src/services/api.js
@@ -0,0 +1,50 @@
+const getProducts = getProductsFn
+function getProductsFn() {
+    return (
+      fetch("https://dummyjson.com/products")
+      .then(resp => resp.json())
+      .then(data => data.products)
+      .catch(err => console.error("Error fetching products:", err))
+    )
+}
+
+const addProducts = addProductsFn
+function addProductsFn(product) {
+    return (
+      fetch("https://dummyjson.com/products/add", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(product)
+      })
+      .then(resp => resp.json())
+      .catch(err => console.error("Error adding product:", err))
+    )
+}
+
+const updateProduct = updateProductFn
+function updateProductFn(id, product) {
+    return (
+      fetch(`https://dummyjson.com/products/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(product)
+      })
+      .then(resp => resp.json())
+      .catch(err => console.error("Error updating product:", err))
+    )
+}
+
+const deleteProduct = deleteProductFn
+function deleteProductFn(id) {
+    return (
+      fetch(`https://dummyjson.com/products/${id}`, {
+        method: "DELETE"
+      })
+      .then(resp => resp.json())
+      .catch(err => console.error("Error deleting product:", err))
+    )
+}
