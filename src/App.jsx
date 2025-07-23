1133bab5f10cc9785aae819b28263f47392c64aa
mumira-bit <mumiraesther41@gmail.com>
2025-07-23 23:45:49 +0300

    Update pp.jsx: route fixes and component setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Route } from 'react-router-dom'
-import { BrowserRouter as Router, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductList from './components/ProductList';
+import ProductDetail from './components/pages/ProductDetail';
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Router>
+    
+      <div className="app">
+        <h1 className="app-container">Inventory Manager</h1>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<ProductList />} />
           <Route path="/products/:id" element={<ProductDetail />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="*" element={<NotFound />} />
         </Routes>
-      </Router>
-      <Footer />
-    </div>
+      </div>
+    
   );
 }
 
